4b53eea49e9f3cd68d4ea9a6795093bf5810af28
renyujuan <363886121@qq.com>
2020-11-17 23:51:25 +0800

    feat: postslist

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from '../features/posts/postsSlice';
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    // This tells Redux that we want our top-level state object to have a field named posts inside, and all
+    // the data for state.posts will be updated by the postsReducer function when actions are dispatched.
+    // state.posts
+    // actions are dispatched, postsReducer function will be called, then state.posts will be updated
+    // so that the postsReducer is being passed as a reducer field named posts
+    posts: postsReducer
+  },
 })
